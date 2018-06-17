d27a9f14a0ea9a67f41bd7fa7bfdd54291fb35e8
Davo <39127468+Davliv@users.noreply.github.com>
2018-06-17 10:53:31 +0400

    Add files via upload

---
+++ task.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	const int length = 3;
+
+	int **arr = new int*[length];
+
+
+	for (int i = 0; i < length; ++i) {
+		arr[i] = new int[length];
+	}
+
+	for (int i = 0; i<length; ++i) {
+		for (int j = 0; j < length; ++j) {
+			std::cout << &arr[i][j];
+		}
+		std::cout << std::endl;
+	}
+	for (int i = 0; i < length; ++i) {
+		delete[] arr[i];
+	}
+	delete[] arr;
+
+}
